137b591e78c35aa767fad76c2b5227567219d952
Irene <irenealonsocordero@gmail.com>
2022-10-20 20:24:23 +0200

    Añadidas las clases de PruebaCSV y el LectorCSV, sin probar

---
+++ src/main/java/com/pruebaCSV/PruebaCSV.java
@@ -0,0 +1,17 @@
+package com.pruebaCSV;
+
+/*
+    Ejemplo muy sencillo de acceso a un archivo .csv desde java con maven
+    No utiliza ningún gestor de dependencias
+ */
+
+public class PruebaCSV {
+    /**
+     * @author Irene Alonso
+     */
+
+    public static void main(String[] args) {
+        //LectorCSV miLector = new LectorCSV();
+        //miLector.leeCSV("./datos/pokemon.csv");
+    }
+}
